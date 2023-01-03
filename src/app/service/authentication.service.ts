7a68397366769aa1b136a8d8f3bf4d11d2609816
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-03 10:22:27 -0300

    implemented: login function at authentication.service

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
+import { environment } from 'src/environment/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  private host = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>(
+      `${this.host}/user/login`,
+      user,
+      { observe: 'response' }
+    );
+  }
 }
