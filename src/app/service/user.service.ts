83c136ce3045457bcc860e8331b536a2393cdbce
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-04 08:20:58 -0300

    implemented: getUsers function at user.service

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
+import { environment } from 'src/environment/environment';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private host = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getUsers(): Observable<User[] | HttpErrorResponse> {
+    return this.http.get<User[]>(`${this.host}/users/list`)
+  }
 }
