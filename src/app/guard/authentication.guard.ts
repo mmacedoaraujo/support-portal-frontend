33dde80a172ea6e1022a66a07ade9eb421cf8403
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-06 12:48:16 -0300

    implemented: isUserLoggedIn function

--- src/app/guard/authentication.guard.ts
+++ src/app/guard/authentication.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+  constructor(private authenticationservice: AuthenticationService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return true;
   }
-  
+
+  private isUserLoggedIn(): boolean {
+    if (this.authenticationservice.isLoggedIn()) {
+      return this.isUserLoggedIn();
+    }
+
+    this.router.navigate(['/login']);
+    //TODO send notification to user
+    return false;
+  }
+
 }
