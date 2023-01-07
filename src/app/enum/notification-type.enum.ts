6ca1a00be7e60add56e4087b17131039313722dd
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-07 18:32:41 -0300

    created: notification-type.enum.ts

---
+++ src/app/enum/notification-type.enum.ts
@@ -0,0 +1,8 @@
+export enum NotificationType {
+  DEFAULT = 'default',
+  SUCCESS = 'success',
+  ERROr = 'error',
+  INFO = 'info',
+  WARNING = 'warning',
+
+}
--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { NotifierService } from 'angular-notifier';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private notifier: NotifierService) {}
 }
