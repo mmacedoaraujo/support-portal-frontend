af008f22e7106012179cfae8dcc7e2413c93bc75
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-09 12:25:19 -0300

    implemented: login logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,9 @@
+import { NotificationType } from './../enum/notification-type.enum';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from '../model/user';
 import { AuthenticationService } from '../service/authentication.service';
 import { NotificationService } from '../service/notification.service';
 
@@ -9,6 +13,8 @@ import { NotificationService } from '../service/notification.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit, OnDestroy {
+  public showLoading: boolean = true;
+  private subscriptions: Subscription[] = [];
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService,
@@ -17,10 +23,38 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     if (this.authenticationService.isLoggedIn()) {
-      this.router.navigateByUrl('/users/management')
+      this.router.navigateByUrl('/users/management');
     } else {
-      this.router.navigateByUrl('/register');
+      this.router.navigateByUrl('/login');
     }
+  }
+
+  public onLogin(user: User): void {
+    this.showLoading = true;
+    console.log(user);
+    this.subscriptions.push(
+      this.authenticationService
+        .login(user)
+        .subscribe((response: HttpResponse<User> | any) => {
+          const token: any = response.headers.get('Jwt-Token');
+          this.authenticationService.saveToken(token);
+          this.authenticationService.addUserToLocalCache(response.body);
+          this.router.navigateByUrl('/users/management');
+          this.showLoading = false;
+        },
+        (errorResponse: HttpErrorResponse) => {
+          console.log(error);
+          this.sendErrorNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.showLoading = false;
+        }
+    );
+  }
+  sendErrorNotification(notiticationType: NotificationType, message: string) {
+   if (message) {
+    this.noitificationService.showNotification(notiticationType, message);
+   }else {
+    this.noitificationService.showNotification(notiticationType, 'AN ERROR OCURRED, PLEASE TRY AGAIN');
+   }
   }
 
   ngOnDestroy(): void {}
--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -20,12 +20,10 @@ export class AuthenticationService {
 
   constructor(private http: HttpClient) {}
 
-  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
-    return this.http.post<HttpResponse<any> | HttpErrorResponse>(
-      `${this.host}/users/login`,
-      user,
-      { observe: 'response' }
-    );
+  public login(user: User): Observable<HttpResponse<User> | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/users/login`, user, {
+      observe: 'response',
+    });
   }
 
   public register(user: User): Observable<User | HttpErrorResponse> {
