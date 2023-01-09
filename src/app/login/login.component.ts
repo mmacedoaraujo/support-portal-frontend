45aae7df7442b09e16379e995cb8ffb350156da1
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-09 11:56:27 -0300

    implemented: login verification

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../service/authentication.service';
+import { NotificationService } from '../service/notification.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
+export class LoginComponent implements OnInit, OnDestroy {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private noitificationService: NotificationService
+  ) {}
 
   ngOnInit(): void {
+    if (this.authenticationService.isLoggedIn()) {
+      this.router.navigateByUrl('/users/management')
+    } else {
+      this.router.navigateByUrl('/register');
+    }
   }
 
+  ngOnDestroy(): void {}
 }
