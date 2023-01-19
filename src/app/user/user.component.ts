21cc8a38f90075c7ecb613c5b0985993b6ca7c5a
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-19 11:48:36 -0300

    added: main section html

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
+  styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   private titleSubject = new BehaviorSubject<string>('Users');
   public titleAction$ = this.titleSubject.asObservable();
 
   constructor() {}
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   public changeTitle(title: string): void {
     this.titleSubject.next(title);
